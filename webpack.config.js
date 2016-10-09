44c4d4bbde78efac7bed6b42679f5a1d6b61465f
khayuen <khayuen@iflix.com>
2016-10-09 13:41:18 +0800

    Setup webpack and related dependencies

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var plugins = [
+  new HtmlWebpackPlugin({
+    title: "We are awesome!!",
+    inject: false,
+    template: path.resolve(__dirname, 'index.html')
+  })
+];
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    application: [
+      './index.js'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/app'),
+    filename: '[name].bundle.[chunkhash].js'
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/dist'),
+    historyApiFallback: {
+      index: '/app/index.html'
+    }
+  }
+};
